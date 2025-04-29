71df07d7cba49d9c9077f7341db16cf5a08b52d7
cotorr <156152443+cotorr@users.noreply.github.com>
2025-04-29 14:49:06 +0900

    4-29
    반응형 1450px

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// 페이드 이미지
+let fade = document.querySelectorAll(".slide"); // 여기를 slide로
+let currentFade = 0;
+let fadeInterval = setInterval(nextFade, 5000);
+
+function nextFade() {
+    fade[currentFade].classList.remove("showing"); // showing만 제거
+    currentFade = (currentFade + 1) % fade.length;
+    fade[currentFade].classList.add("showing"); // showing만 추가
+}
