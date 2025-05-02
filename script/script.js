e401d01295b9ca0a9ff3977d63557134fc2e9d75
cotorr <156152443+cotorr@users.noreply.github.com>
2025-05-02 16:52:32 +0900

    5-2
    html 디자인 수정

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 // 페이드 이미지
 let fade = document.querySelectorAll(".slide"); // 여기를 slide로
 let currentFade = 0;
-let fadeInterval = setInterval(nextFade, 5000);
+let fadeInterval = setInterval(nextFade, 3250);
 
 function nextFade() {
     fade[currentFade].classList.remove("showing"); // showing만 제거
